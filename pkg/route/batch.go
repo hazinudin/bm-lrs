4907c13cabb36b4c6658491176c402b35063fdfe
Hannan Azinuddin <hazinuddin@gmail.com>
2026-02-04 11:01:50 +0700

    Implement ViewName, SegmentQuery and Linestring query in LRSRouteBatch

--- pkg/route/batch.go
+++ pkg/route/batch.go
@@ -1,5 +1,10 @@
 package route
 
+import (
+	"fmt"
+	"strings"
+)
+
 type sourceFile struct {
 	filePath string
 	routes   []string
@@ -19,6 +24,10 @@ type LRSRouteBatch struct {
 func (l *LRSRouteBatch) AddRoute(route LRSRoute) {
 	if l.routes == nil {
 		l.routes = make(map[string]LRSRoute)
+	}
+
+	if l.sourceFiles == nil {
+		l.sourceFiles = &batchSourceFiles{}
 	}
 
 	// Check if the route is materialized or not.
@@ -113,4 +122,79 @@ func (l *LRSRouteBatch) Release() {
 	for _, route := range l.routes {
 		route.Release()
 	}
+}
+
+// ViewName returns a query for loading point data from all source files in the batch
+func (l *LRSRouteBatch) ViewName() string {
+	if l.sourceFiles == nil || len(l.sourceFiles.Point) == 0 {
+		return ""
+	}
+
+	var queries []string
+	var noPushDownFiles []string
+	for _, sf := range l.sourceFiles.Point {
+		if len(sf.routes) == 0 {
+			noPushDownFiles = append(noPushDownFiles, sf.filePath)
+		} else {
+			routeList := strings.Join(sf.routes, "','")
+			queries = append(queries, fmt.Sprintf(`SELECT * FROM "%s" WHERE ROUTEID IN ('%s')`, sf.filePath, routeList))
+		}
+	}
+
+	if len(noPushDownFiles) > 0 {
+		noPushDownQuery := fmt.Sprintf(`SELECT * FROM read_parquet([%s])`, strings.Join(noPushDownFiles, ", "))
+		queries = append(queries, noPushDownQuery)
+	}
+
+	return fmt.Sprintf("(%s)", strings.Join(queries, " UNION ALL "))
+}
+
+// SegmentQuery returns a query for loading segment data from all source files in the batch
+func (l *LRSRouteBatch) SegmentQuery() string {
+	if l.sourceFiles == nil || len(l.sourceFiles.Segment) == 0 {
+		return ""
+	}
+
+	var queries []string
+	var noPushDownFiles []string
+	for _, sf := range l.sourceFiles.Segment {
+		if len(sf.routes) == 0 {
+			noPushDownFiles = append(noPushDownFiles, sf.filePath)
+		} else {
+			routeList := strings.Join(sf.routes, "','")
+			queries = append(queries, fmt.Sprintf(`SELECT * FROM "%s" WHERE ROUTEID IN ('%s')`, sf.filePath, routeList))
+		}
+	}
+
+	if len(noPushDownFiles) > 0 {
+		noPushDownQuery := fmt.Sprintf(`SELECT * FROM read_parquet([%s])`, strings.Join(noPushDownFiles, ", "))
+		queries = append(queries, noPushDownQuery)
+	}
+
+	return strings.Join(queries, " UNION ALL ") + ";"
+}
+
+// LinestringQuery returns a query for loading linestring data from all source files in the batch
+func (l *LRSRouteBatch) LinestringQuery() string {
+	if l.sourceFiles == nil || len(l.sourceFiles.LineString) == 0 {
+		return ""
+	}
+
+	var queries []string
+	var noPushDownFiles []string
+	for _, sf := range l.sourceFiles.LineString {
+		if len(sf.routes) == 0 {
+			noPushDownFiles = append(noPushDownFiles, sf.filePath)
+		} else {
+			routeList := strings.Join(sf.routes, "','")
+			queries = append(queries, fmt.Sprintf(`SELECT * FROM "%s" WHERE ROUTEID IN ('%s')`, sf.filePath, routeList))
+		}
+	}
+
+	if len(noPushDownFiles) > 0 {
+		noPushDownQuery := fmt.Sprintf(`SELECT * FROM read_parquet([%s])`, strings.Join(noPushDownFiles, ", "))
+		queries = append(queries, noPushDownQuery)
+	}
+
+	return strings.Join(queries, " UNION ALL ") + ";"
 }
