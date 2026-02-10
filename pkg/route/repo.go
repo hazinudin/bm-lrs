2e54445b2ad976e5a53d9d08d4c3fbca92d7ac15
Hannan Azinuddin <hazinudin@hotmail.com>
2026-02-10 17:50:49 +0700

    Set the fetch limit to 10

--- pkg/route/repo.go
+++ pkg/route/repo.go
@@ -36,7 +36,7 @@ func NewLRSRouteRepository(connector *duckdb.Connector, pgConnStr string, db *sq
 		db:                db,
 		tokenURL:          "https://gisportal.binamarga.pu.go.id/portal/sharing/rest/generateToken",
 		featureServiceURL: "https://gisportal.binamarga.pu.go.id/arcgis/rest/services/Jalan/BinaMargaLRS/MapServer/0/query",
-		arcgisFetchLimit:  250, // Feature query limit
+		arcgisFetchLimit:  10, // Feature query limit
 	}
 }
 
